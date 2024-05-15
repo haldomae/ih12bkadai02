64badc4528b2aa645858b00e457dd5499073a510
haldomae <hal.domae@gmail.com>
2024-05-15 12:26:24 +0900

    画面の要素を取得

--- app/src/main/java/com/ih13b312_domaekazuki/ih13bkadai02/MainActivity.kt
+++ app/src/main/java/com/ih13b312_domaekazuki/ih13bkadai02/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ih13b312_domaekazuki.ih13bkadai02
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +19,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    val messageView: TextView = findViewById(R.id.messageView)
+    val waterBtn: Button = findViewById(R.id.waterBtn)
 }
